bbda1a052c27c2d2f71d3f67867e3b0c2f438d3b
hmharshith <harshith.hm@thedatateam.in>
2018-04-14 21:52:41 +0530

    Basic GET request

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ const tools = require("../public/javascripts/tools");
 
 var db = tools.getFireStore();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", function (req, res) {
 
     res.setHeader('Content-Type', 'application/json');

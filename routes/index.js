a2c9d0825389cc5eb249247fd63139eba9505f44
harshvit <hmharshith@gmail.com>
2018-04-21 01:53:40 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,10 @@ app.get("/", function (req, res) {
     res.send({
         "Project" : "Real time weather processor \n" ,
         "description" : "Final Sem project by Harshith HM (14BEC0393) \n" ,
-        "request" : "Send POST request to /upload to upload weather data to FireStore \n"
+        "Your Request" : "Temperature"+req.query.temp ,
+        "ToDo" : "Send POST request to /upload to upload weather data to FireStore \n"
     })
 });
-
 
 app.post("/upload", function (req, res) {
     temp = req.headers.temperature;
